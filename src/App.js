7362ad80112f48c2a48fcb9dd08338a36d323471
nivaldohydalgo <nivaldohydalgo@gmail.com>
2020-06-11 11:19:45 -0300

    Commit projeto limpo

--- src/App.js
+++ src/App.js
@@ -2,23 +2,13 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Nivaldo
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <p>
+                Teste do REACT
+            </p>
+        </div>
+    );
 }
 
 export default App;
